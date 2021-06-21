f1f08c2bd07ab90b7d2c0be231540ea5088bb9e4
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-06-21 23:06:33 +0700

    on progress 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,26 @@ import { LinearProgress } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import { AddShoppingCartTwoTone } from '@material-ui/icons';
 import { Badge } from '@material-ui/core';
+import { useQuery } from 'react-query';
 
+export type CartItemType = {
+  id: number,
+  category: string
+  description: string
+  image: string
+  price: string
+  amount: string
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products', getProducts);
+    console.log(222, data);
+    
+    return <div className='App'>Start</div>
 }
 
 export default App;
